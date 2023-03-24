f1d8f7a76502d171926bf0e7a1b38b58ea3df574
Ben Gundersen <ben@bengundersen.com>
2023-03-24 12:41:50 -0400

    minor fixes, speech still doens't work on mobile

--- src/App.js
+++ src/App.js
@@ -23,13 +23,13 @@ class App extends React.Component {
         'lang': 'en-GB',
         'rate': 1,
         'pitch': 1,
-        'voice':'Google UK English Male',
-        'splitSentences': true
+        'voice':'Google UK English Male'
+        //'splitSentences': true
     }).then((data) => {
       // The "data" object contains the list of available voices and the voice synthesis params
       console.log("Speech is ready, voices are available", data)
     }).catch(e => {
-      console.error("An error occured while initializing : ", e)
+      alert("An error occured while initializing speech : ", e)
     })
 
     console.log("env", process.env);
